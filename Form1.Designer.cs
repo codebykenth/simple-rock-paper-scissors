c9e98713f66ab7ca21b45781db6f597f0a395c4e
kenth <kenth@LAPTOP-832N4QBD>
2023-10-01 20:49:39 +0800

    Create the UI for the game.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,13 +23,232 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pbQuestion = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.lblPlayerScoreValue = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.lblComputerScoreValue = new System.Windows.Forms.Label();
+            this.lblComputer = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQuestion)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRock
+            // 
+            this.btnRock.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnRock.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRock.Location = new System.Drawing.Point(3, 3);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(215, 178);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.Text = "ROCK";
+            this.btnRock.UseVisualStyleBackColor = false;
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnPaper.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaper.Location = new System.Drawing.Point(445, 3);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(215, 178);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.Text = "PAPER";
+            this.btnPaper.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(224, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(215, 178);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "SCISSORS";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // pbQuestion
+            // 
+            this.pbQuestion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pbQuestion.ImageLocation = "C:\\Users\\kenth\\source\\repos\\RockPaperScissors\\images\\question2.png";
+            this.pbQuestion.Location = new System.Drawing.Point(211, 161);
+            this.pbQuestion.Margin = new System.Windows.Forms.Padding(0);
+            this.pbQuestion.Name = "pbQuestion";
+            this.pbQuestion.Size = new System.Drawing.Size(255, 231);
+            this.pbQuestion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbQuestion.TabIndex = 3;
+            this.pbQuestion.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnRock);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.btnPaper);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(9, 422);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(666, 185);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.flowLayoutPanel2.Controls.Add(this.splitContainer1);
+            this.flowLayoutPanel2.Controls.Add(this.splitContainer2);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(681, 136);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(513, 471);
+            this.flowLayoutPanel2.TabIndex = 5;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(139, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(951, 108);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "Rock Paper Scissors";
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer.Location = new System.Drawing.Point(65, 106);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(96, 25);
+            this.lblPlayer.TabIndex = 0;
+            this.lblPlayer.Text = "PLAYER";
+            // 
+            // lblPlayerScoreValue
+            // 
+            this.lblPlayerScoreValue.AutoSize = true;
+            this.lblPlayerScoreValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerScoreValue.Location = new System.Drawing.Point(107, 84);
+            this.lblPlayerScoreValue.Name = "lblPlayerScoreValue";
+            this.lblPlayerScoreValue.Size = new System.Drawing.Size(68, 73);
+            this.lblPlayerScoreValue.TabIndex = 1;
+            this.lblPlayerScoreValue.Text = "0";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lblPlayer);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lblPlayerScoreValue);
+            this.splitContainer1.Size = new System.Drawing.Size(507, 234);
+            this.splitContainer1.SplitterDistance = 216;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.splitContainer2.Location = new System.Drawing.Point(3, 243);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.lblComputer);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.lblComputerScoreValue);
+            this.splitContainer2.Size = new System.Drawing.Size(507, 224);
+            this.splitContainer2.SplitterDistance = 216;
+            this.splitContainer2.TabIndex = 7;
+            // 
+            // lblComputerScoreValue
+            // 
+            this.lblComputerScoreValue.AutoSize = true;
+            this.lblComputerScoreValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputerScoreValue.Location = new System.Drawing.Point(107, 75);
+            this.lblComputerScoreValue.Name = "lblComputerScoreValue";
+            this.lblComputerScoreValue.Size = new System.Drawing.Size(68, 73);
+            this.lblComputerScoreValue.TabIndex = 2;
+            this.lblComputerScoreValue.Text = "0";
+            // 
+            // lblComputer
+            // 
+            this.lblComputer.AutoSize = true;
+            this.lblComputer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputer.Location = new System.Drawing.Point(45, 101);
+            this.lblComputer.Name = "lblComputer";
+            this.lblComputer.Size = new System.Drawing.Size(132, 25);
+            this.lblComputer.TabIndex = 1;
+            this.lblComputer.Text = "COMPUTER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1205, 614);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.pbQuestion);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1221, 653);
+            this.MinimumSize = new System.Drawing.Size(1221, 653);
+            this.Name = "Form1";
+            this.Text = "ROCK PAPER SCISSORS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbQuestion)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pbQuestion;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblPlayer;
+        private System.Windows.Forms.Label lblPlayerScoreValue;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Label lblComputer;
+        private System.Windows.Forms.Label lblComputerScoreValue;
     }
 }
 
